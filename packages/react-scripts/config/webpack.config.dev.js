eb05e2b8a68e8079511f069cba77d803aa9d25d2
Deseteral <deseteral@gmail.com>
2017-09-30 20:16:39 +0200

    PostCSS SVG import

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -191,6 +191,7 @@ module.exports = {
                       ],
                       flexbox: 'no-2009',
                     }),
+                    require('postcss-inline-svg'),
                   ],
                 },
               },
