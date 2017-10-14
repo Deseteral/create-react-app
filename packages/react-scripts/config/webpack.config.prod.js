a92e24162ac950b95db89e8f2429e51ccb5cdb30
Deseteral <deseteral@gmail.com>
2017-10-14 14:38:32 +0200

    SERVICE_URL global

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -287,7 +287,11 @@ module.exports = {
     // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
     // It is absolutely essential that NODE_ENV was set to production here.
     // Otherwise React will be compiled in the very slow development mode.
-    new webpack.DefinePlugin(env.stringified),
+    new webpack.DefinePlugin(Object.assign(
+      {},
+      env.stringified,
+      { SERVICE_URL: process.env.SERVICE_URL }
+    )),
     // Minify the code.
     new webpack.optimize.UglifyJsPlugin({
       compress: {
